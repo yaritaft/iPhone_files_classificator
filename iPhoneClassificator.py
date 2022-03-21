0505abc2b25b73afc0d0689c4cb18b4d5ad33a96
Yari Taft <yari.taft@gmail.com>
2022-03-20 22:49:26 -0300

    Translated everything to english.

--- iPhoneClassificator.py
+++ iPhoneClassificator.py
@@ -4,16 +4,16 @@ import shutil
 
 base_dir = os.getcwd()
 
-def checkear_si_existe_carpeta(nombre_de_la_carpeta):
-    return os.path.isdir(os.path.join(base_dir, nombre_de_la_carpeta))
+def check_folder_existance(folder_name):
+    return os.path.isdir(os.path.join(base_dir, folder_name))
 
-def move_to_folder(nombre_de_la_carpeta, archivos_a_mover):
-    if not checkear_si_existe_carpeta(nombre_de_la_carpeta):
-        os.mkdir(nombre_de_la_carpeta)
-    for un_archivo in archivos_a_mover:
-        full_path = os.path.join(base_dir, un_archivo)
-        un_archivo_sin_su_carpeta_padre = un_archivo.replace("unclassifiedPhotosAndVideos\\", "")
-        new_full_path = os.path.join(base_dir, nombre_de_la_carpeta, un_archivo_sin_su_carpeta_padre)
+def move_to_folder(folder_name, files_to_classify):
+    if not check_folder_existance(folder_name):
+        os.mkdir(folder_name)
+    for one_file in files_to_classify:
+        full_path = os.path.join(base_dir, one_file)
+        file_without_parent_folder = one_file.replace("unclassifiedPhotosAndVideos\\", "")
+        new_full_path = os.path.join(base_dir, folder_name, file_without_parent_folder)
         shutil.move(full_path, new_full_path)
 
 def process_files_matching_with_regex(destination_folder, regex ):
@@ -25,10 +25,12 @@ def process_files_matching_with_regex(destination_folder, regex ):
     print("================")
 
 
+# The order we execute this process files is critical since some regex assumes that previous cases are already classified.
+# order critical first wpp then camera
 process_files_matching_with_regex("whatsapp-images", "*_*_*.JPG")
 process_files_matching_with_regex("camera", "*_*.JPG")
 process_files_matching_with_regex("camera-multi", "*.MOV")
 process_files_matching_with_regex("screenshots", "*.PNG")
+# order critical first wpp then tiktoks
 process_files_matching_with_regex("whatsapp_videos_and_screenrecords", "*_*.MP4")
-# # MOVER TODO LO DE WPP Y GRABACIONES DE PANTALLA PREVIO A BUSCAR EL RESTO DE .MP4 QUE VAN A SER A TIKTOKS
 process_files_matching_with_regex("tiktoks", "*.MP4")
